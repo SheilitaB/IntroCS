91eb6b1cd4f25a1e60d23e6f8e1217cd6dd79886
SheilitaB <18sbrito@iaa.edu.ec>
2015-05-08 07:27:05 -0500

    HelloWorld.py

---
+++ HelloWorld.py
@@ -0,0 +1,11 @@
+print "Hello World"
+print "My name is Sheila"
+print "I am 15 years old \nThis is phython"
+print "2015"
+print 2015 + 1 #this is for a comment the hashtag
+print 2015 - 3
+print 2015 / 5
+print 2015 * 2
+print "Odd or even number"
+print 17 % 3
+print 16 % 2
