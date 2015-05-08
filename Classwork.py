7764411102c12deb8d88f85fdadd2243003873b7
SheilitaB <18sbrito@iaa.edu.ec>
2015-05-08 07:32:49 -0500

    Classwork.py

---
+++ Classwork.py
@@ -0,0 +1,9 @@
+mystring = 'Hello'
+myfloat = 10
+myint = 20
+print  mystring, 'My name is Sheila and I am', myfloat, 'yeasrs old, and in ten yeasrs i will be', myint
+print myfloat + myint
+print myint/myfloat
+print myfloat*myint
+print myint-myfloat
+print myint + myfloat*myint
