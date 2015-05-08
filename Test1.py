13fd2d94aed4e02a210b12a37f4c27672d53026c
SheilitaB <18sbrito@iaa.edu.ec>
2015-05-08 07:34:37 -0500

    Test1.py

---
+++ Test1.py
@@ -0,0 +1,10 @@
+operand1 = 95
+operand2 = 64.5 
+#operations
+print operand1 + operand2
+print operand1 - operand2
+print operand1 * operand2
+print operand1 / operand2
+print operand1 % operand2
+#This method is useful because you dont need to rewrite the numbers,
+#you just change the variables and the result will automaticly appear.
