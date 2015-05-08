512ade4cb3d8b55229fca8e88aa16acbf397baae
SheilitaB <18sbrito@iaa.edu.ec>
2015-05-08 07:35:00 -0500

    Variables.py

---
+++ Variables.py
@@ -0,0 +1,24 @@
+name = "Sheila Brito"
+age = 15
+gpa = 4.0
+
+print name
+print age
+print gpa
+print 'My name is', name, 'I am', age, 'years old. My gpa is', gpa
+print 'in ten years i will be', age+10
+
+ageinhumanyears = 3
+ageindogyears = ageinhumanyears * 7
+
+print 'My dog is', ageindogyears, 'years old in dog years'
+print 'but my dog is', ageinhumanyears, 'in human years'
+
+#order of operations
+#parenthesis, exponentes, multiplication, division, addition, substraction
+mynumber = 5*4+10
+print mynumber
+print "hello", 2
+import math
+root= math. sqrt(81)
+print root
