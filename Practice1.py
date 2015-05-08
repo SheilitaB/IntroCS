8dfd7e1d88ac8a16a4a91c13a7d0920d2fb9baec
SheilitaB <18sbrito@iaa.edu.ec>
2015-05-08 07:33:30 -0500

    Practice1.py

---
+++ Practice1.py
@@ -0,0 +1,14 @@
+name = "Sheila"
+lastname = 'Brito'
+adress = 'Terra Sol'
+email = 'sheilita2000@hotmail.com'
+phonenumber = '0989577944'
+
+print name
+print lastname
+print adress
+print email
+print phonenumber
+
+print "Hi, my name is", name, "and my last name is", lastname, "I live in", adress, "and my email is", email, "but if you want to call me, this is my number:", phonenumber,"thanks"
+
