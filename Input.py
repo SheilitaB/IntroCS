9f68ca55f4a75142a144f67027183a3661534141
SheilitaB <18sbrito@iaa.edu.ec>
2015-05-20 10:30:36 -0500

    Input.py

---
+++ Input.py
@@ -0,0 +1,9 @@
+name = raw_input('What is your name?')
+last = raw_input('What is your last name?')
+
+print 'your name is', name, last
+
+age = input('How old are you?')
+print 'you are', age, 'years old'
+print age * 7
+print name * 7
