20f30501500fc522ec9f11756f37962dc751ec00
SheilitaB <18sbrito@iaa.edu.ec>
2015-05-22 09:07:40 -0500

    Practice2.py

---
+++ Practice2.py
@@ -0,0 +1,19 @@
+#Create a form using input and raw_input
+#name
+#last name
+#year of birth
+#Favorite singer band
+#favorite movie
+#best fiend
+#calculate current age 
+#print everything in a string concatenating variables
+#print the name 10 times
+name = raw_input('What is your name?')
+lastname = raw_input('What is last your name?')
+yearofbirth = input('When were you born?')
+favoritesingerorband = raw_input('What is your favorite singer or band?')
+favoritemovie = raw_input('What is your favorite favorite movie?')
+bestfriend = raw_input('Who is your best friend?')
+print 'You are', 2015- yearofbirth, 'years old '
+print 'your name is', name, lastname, ".You were born in", yearofbirth, 'Your favorite singer or band is', favoritesingerorband, 'Your favorite movie is', favoritemovie, 'and your best friend is', bestfriend
+print name*10
